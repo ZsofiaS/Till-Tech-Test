d61f5892c86b8b879df189764662dc7c33917e7a
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-19 13:54:30 +0100

    Add enterName function to Till class

--- spec/TillSpec.js
+++ spec/TillSpec.js
@@ -7,4 +7,15 @@ describe('Till', function() {
     let till = new Till();
     expect(till.items.length).toEqual(0);
   })
+  it('stores the customers name, by default empty', function() {
+    let till = new Till();
+    expect(till.name).toEqual("");
+  })
+  describe('#enterName', function() {
+    it('will store the customers name', function() {
+      let till = new Till();
+      till.enterName("Zso");
+      expect(till.name).toEqual("Zso");
+    })
+  })
 })
