d75ac11c709696d7495ead9d1c34170401be5e56
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-21 15:20:24 +0100

    Refactor and add validation

--- spec/TillSpec.js
+++ spec/TillSpec.js
@@ -11,12 +11,6 @@ describe('Till', function() {
     let till = new Till();
     expect(till.name).toEqual("");
   })
-  it('stores the date', function() {
-    spyOn(Date, 'now').and.returnValue(1592574130802);
-    let till = new Till();
-    till.order("Tiramisu", 2)
-    expect(till.date).toEqual("6/19/2020, 2:42:10 PM")
-  })
   describe('#enterName', function() {
     it('will store the customers name', function() {
       let till = new Till();
@@ -32,14 +26,19 @@ describe('Till', function() {
         "Tiramisu": 1
       })
     })
+    it('throws error if item is not in menu or number given is invalid', function() {
+      let till = new Till();
+      expect(till.order("Taramusu", "one")).toEqual("Invalid item or number")
+    })
   })
   describe('#printReceipt', function() {
     it('prints the receipt containing date & name of cafe & order', function() {
       spyOn(Date, 'now').and.returnValue(1592574130802);
+      let date = new Date(1592574130802).toLocaleString("en-IE")
       let till = new Till();
       till.enterName("Zso");
       till.order("Americano", 1);
-      expect(till.printReceipt()).toEqual(`${till.date}\nThe Coffee Connection\n\n123 Lakeside Way\nPhone: 16503600708\n\nZso\n Americano 1 x 3.75\n\nTax: 0.32\nTotal: 4.07\n\nThank you!`)
+      expect(till.printReceipt()).toEqual(`${date}\nThe Coffee Connection\n\n123 Lakeside Way\nPhone: 16503600708\n\nZso\n Americano 1 x 3.75\n\nTax: $0.32\nTotal: $4.07\n\nThank you!`)
     })
   })
 })
